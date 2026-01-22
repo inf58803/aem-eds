b6b81a67967f176fe17a96aa5b22a2aa862bb640
inf58803 <inf58803@adobe.com>
2026-01-22 14:18:57 +0530

    updated artcile.js

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -9,6 +9,7 @@ export default async function decorate(block) {
         const li = document.createElement('li');
         li.innerHTML= `
           <a href="${item.path}">
+          <img src="${item.image}" alt="${item.title}" loading="lazy" >
             <h3>${item.title}</h3>
             <p>${item.description}</p>
         `;
